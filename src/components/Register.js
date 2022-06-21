7a38bab7e4b537cce1818461327d80d3c8fc0380
roshanak <roshanak.ahmed11@gmail.com>
2022-06-22 00:01:02 +0600

    login register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,26 +1,38 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState }  from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Register() {
   // ! Using react router to navigate
   const navigate = useNavigate()
-
   // ! Put our form fields in state.
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-    passwordConfirmation: "",
-    email: "",
+  const [formData, setFormData] = React.useState({
+    username: 'rosh',
+    email: 'rosh@rosh.com',
+    password: 'rosh123',
+    passwordConfirmation: 'rosh123',
+  })
+
+  // !Errors
+  const [errors, setErrors] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
   })
 
   function handleChange(e) {
-    // ! name: field you've typed in, e.g. the email input
-    // ! value: the text that's in that field
-    const { name, value } = e.target
+    // ! name: The field you've typed in, e.g. username/email/password.
+    // ! value: the text that's in that field.
+    // console.log(e)
+    const { name, value } = e.target // ! This will get the name and value out of the event object. The name is the 'username' (example) and value is whatever's in the input.
     setFormData({
-      ...formData, // ! This is whatever the form data was before, all it's fields.
-      [name]: value, 
+      ...formData, // ! This is whatever the form data was before, all its fields.
+      [name]: value,
+    })
+    setErrors({
+      ...errors,
+      [name]: '',
     })
   }
 
@@ -28,18 +40,21 @@ export default function Register() {
     e.preventDefault()
 
     try {
-      const { data } = await axios.post('/api/register', formData)
+      const { data } = await axios.post('/api/register', formData) //*axios is an interface over the fetch api, it gives you back a response object with data inside it.
       console.log(data)
-      // ! Navigate to the /login page. 
+      // ! Navigate to the /login page.
       navigate('/login')
 
-    } catch (err) {
-      // ! Print out the response from the backend if there's an error
-      console.log(err.response.data)
+    } catch (error) {
+      // ! Print out the response form the backend if there's an error
+      console.log(error.response.data)
+      setErrors(error.response.data.errors)
     }
   }
 
   console.log(formData)
+
+
   return <div className="section">
     <div className="container">
       <form onSubmit={handleSubmit}>
@@ -49,11 +64,13 @@ export default function Register() {
             <input
               className="input"
               type="text"
-              name={'username'} 
-              // ! Adding these 2 fields means your component is 'controlled'. This is good practice!!
+              name={'username'}
+              // ! Adding these 2 fields means your component is 'controlled.'
               value={formData.username}
-              onChange={handleChange}        
+              onChange={handleChange}
             />
+            {/* // ! Really nice custom error message */}
+            {errors.username && <small className="has-text-danger">{errors.username}</small>}
           </div>
         </div>
         <div className="field">
@@ -63,9 +80,12 @@ export default function Register() {
               className="input"
               type="text"
               name={'email'}
+              // ! Adding these 2 fields means your component is 'controlled.'
               value={formData.email}
               onChange={handleChange}
             />
+            {/* // ! Really nice custom error message */}
+            {errors.email && <small className="has-text-danger">{errors.username}</small>}
           </div>
         </div>
         <div className="field">
@@ -75,9 +95,12 @@ export default function Register() {
               className="input"
               type="password"
               name={'password'}
+              // ! Adding these 2 fields means your component is 'controlled.'
               value={formData.password}
               onChange={handleChange}
             />
+            {/* // ! Really nice custom error message */}
+            {errors.password && <small className="has-text-danger">{errors.username}</small>}
           </div>
         </div>
         <div className="field">
@@ -87,9 +110,12 @@ export default function Register() {
               className="input"
               type="password"
               name={'passwordConfirmation'}
+              // ! Adding these 2 fields means your component is 'controlled.'
               value={formData.passwordConfirmation}
               onChange={handleChange}
             />
+            {/* // ! Really nice custom error message */}
+            {errors.passwordConfirmation && <small className="has-text-danger">{errors.username}</small>}
           </div>
         </div>
         <button className="button">Submit</button>
