c90b6ec4fb32f2453824b95e7fb356ca6e0bb56d
roshanak <roshanak.ahmed11@gmail.com>
2022-06-23 07:45:13 +0600

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,34 +11,34 @@ function Navbar() {
       {/* Lines below: Navbar is created in Header and has Home, Plants, Show Plant, Login, and Register tabs that link to the Home.js, PlantIndex.js, PlantShow.js, Login.js, and Register.js components. */}
       <header>
         {/* Lines below: "navbar is-dark", "container", "navbar-brand", "navbar-item" imports class names from bulma */}
-        <nav className="navbar is-dark">
+        <nav className="navbar is-light">
           <div className="container">
             <div className="navbar-brand">
               {/*Line below: Link to="/" creates default page as Home page */}
               <Link to="/" className="navbar-item">
                 {/* Line below: Home tab */}
-                <h1>Home</h1>
+                <h2>Home</h2>
               </Link>
               <Link to="/plants" className="navbar-item">
                 {/* Line below: PlantIndex.js. tab */}
-                <h1>Plants</h1>
+                <h2>Plants</h2>
               </Link>
               <Link to="/plantcare" className="navbar-item">
                 {/* Line below: PlantCare.js tab */}
-                <h1>Plant Care</h1>
+                <h2>Plant Care</h2>
               </Link>
               {/* Line below: Link to="/plants/:plantId" creates a link params variable that changes based on which plant name button we click on. Example: if we click on Cactus, the link will be /plants/cactus */}
               <Link to="/login" className="navbar-item">
                 {/* Line below: Login.js tab */}
-                <h1>Login</h1>
+                <h2>Login</h2>
               </Link>
               <Link to="/register" className="navbar-item">
                 {/* Line below: Register.js tab */}
-                <h1>Register</h1>
+                <h2>Register</h2>
               </Link>
               <Link to="/Cart" className="navbar-item">
                 {/* Line below: Cart.js tab */}
-                <h1>Cart</h1>
+                <h2>Cart</h2>
               </Link>
             </div>
           </div>
