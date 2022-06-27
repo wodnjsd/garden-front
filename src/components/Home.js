861bfb4b02387f2e2ab105e4bfe13c751554cb1c
robynsm <robynamysmith92@gmail.com>
2022-06-27 22:50:42 +0100

    Added some styling to the Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ const responsive = {
     items: 2,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 400, min: 0 },
     items: 1,
   },
 };
@@ -28,9 +28,18 @@ function Home() {
     <section className="hero is-fullheight-with-navbar">
       <div className="hero-body">
         <div className="container">
-          <p className="title is-1 has-text-centered">
+          <h1 className="title is-1 has-text-centered has-text-success-dark">
             I Beg Your Garden?!
-          </p>
+          </h1>
+          <h4 className="subtitle is-4 has-text-centered has-text-success-dark">
+            Welcome to the jungle!
+          </h4>
+          <div className="container">
+            <img src='https://res.cloudinary.com/dlxbte5xh/image/upload/v1655905671/pexels-samuel-crosland-2557232_qlespz.jpg' alt="banner image" style={{ maxWidth: 1345 }}></img>
+          </div>
+          
+
+          <p className="has-text-success-dark">Some recent purchases...</p>
           <Carousel 
             swipeable={false}
             draggable={false}
@@ -51,7 +60,30 @@ function Home() {
             <div><img src='https://res.cloudinary.com/dlxbte5xh/image/upload/v1656085166/pexels-kulbir-7365049_ze1urb_cfyewy.jpg' alt='English Ivy'></img></div>
             <div><img src='https://res.cloudinary.com/dlxbte5xh/image/upload/v1656085351/pexels-karolina-grabowska-5706224_jq5fgp_ykwos8.jpg' alt='Chinese Money Plant'></img></div>
           </Carousel>
-          
+          <footer className="footer">
+            <div className="content has-text-centered">
+              <div className="container">
+                <a href="">About IBYG</a>
+                <br />
+
+                <a href="">Contact Us</a>
+                <br />
+
+                <a href="">FAQs</a>
+                <br />
+              </div>
+              <p>
+                <strong>IBYG</strong> by <a href="https://jgthms.com">Jeanzle</a>, <a href="https://jgthms.com">Rosh</a>, <a href="https://jgthms.com">Jae</a> and <a href="https://www.linkedin.com/in/robyn-smith-27187910b/">Robyn</a>. Our code is open-sourced and hosted on <a href="https://github.com/wodnjsd/project-3-frontend">GitHub</a>.
+              </p>
+            </div>
+          </footer>
+          {/* <div className="container">
+            <footer className="has-background-dark">
+              <p className="has-text-white">About I Beg Your Garden</p>
+              <p className="has-text-white">Contact Us</p>
+              <p className="has-text-white">FAQs</p>
+            </footer>
+          </div> */}
         </div>
       </div>
     </section>
