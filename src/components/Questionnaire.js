f7ec9710323a78506aa59ba223502cc6f262faec
jeanzless <jeanzle.albalate208@gmail.com>
2022-06-28 19:12:20 +0100

    commit

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import "survey-core/defaultV2.css";
+import { StylesManager, Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+StylesManager.applyTheme("defaultV2");
+
 
 function compareAnswer(){
   let yesCounter = 0;
-  
+
   
 }
 
@@ -11,7 +16,7 @@ function Questionnaire() {
   return (
     <section>
       <h1 className="title is-1 has-text-centered has-text-success-dark">Which Plant are you?</h1>
-      <p className="description">this is a Qustionnaire in which we will be asking you several questions and through our algorithm we will determine which plant you are.</p>
+      <p className="subtitle is-3 has-text-centered has-text-success-dark">this is a Qustionnaire in which we will be asking you several questions and through our algorithm we will determine which plant you are.</p>
     </section>
     
   )
