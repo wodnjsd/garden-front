d0eaa3c7e9b29bfd7d392e0a7ddc94de0422c283
jeanzless <jeanzle.albalate208@gmail.com>
2022-06-30 19:29:27 +0100

    commit Questionnaire

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -1,25 +1,25 @@
-// import React from 'react'
-// import "survey-core/defaultV2.css";
-// import { StylesManager, Model } from "survey-core";
-// import { Survey } from "survey-react-ui";
-// StylesManager.applyTheme("defaultV2");
+import React from 'react'
+import "survey-core/defaultV2.css";
+import { StylesManager, Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+StylesManager.applyTheme("defaultV2");
 
 
-// function compareAnswer(){
-//   let yesCounter = 0;
+function compareAnswer(){
+  let yesCounter = 0;
 
   
-// }
+}
 
-// function Questionnaire() {
-//   const [counter, setCounter] = React.useState(0);
-//   return (
-//     <section>
-//       <h1 className="title is-1 has-text-centered has-text-success-dark">Which Plant are you?</h1>
-//       <p className="subtitle is-3 has-text-centered has-text-success-dark">this is a Qustionnaire in which we will be asking you several questions and through our algorithm we will determine which plant you are.</p>
-//     </section>
+function Questionnaire() {
+  const [counter, setCounter] = React.useState(0);
+  return (
+    <section>
+      <h1 className="title is-1 has-text-centered has-text-success-dark">Which Plant are you?</h1>
+      <p className="subtitle is-3 has-text-centered has-text-success-dark">this is a Qustionnaire in which we will be asking you several questions and through our algorithm we will determine which plant you are.</p>
+    </section>
     
-//   )
-// }
+  )
+}
 
-// export default Questionnaire
+export default Questionnaire
